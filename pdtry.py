610d43146226c7e24a3cfaf1b3dbf9c9eb8b9f1a
Ninad Chaphekar <ninadchaphekar@gmail.com>
2024-04-26 17:50:04 +0530

    .

--- pdtry.py
+++ pdtry.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-dataset = pd.read_csv("train.csv")
+dataset = pd.read_csv("train.csv", index_col=[0,1])
 dataset = dataset.dropna()
 
 num_desirable = dataset[dataset['orig_score'] > 2].shape[0]
 num_undesirable = dataset[dataset['orig_score'] <= 2].shape[0]
 
-print(dataset['orig_score'].dtype())
+print(dataset.columns)
 # print(num_desirable, num_undesirable, num_desirable/num_undesirable)
 
 
